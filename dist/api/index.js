27f568a73529c80507a10034b984702c2d20be7b
YUGAM05 <24002170210108@mail.ljku.edu.in>
2026-05-22 00:57:22 +0530

    fix: relocate serverless handler to Vercel-standard api/index structure to guarantee correct function invocation

--- dist/api/index.js
+++ dist/api/index.js
@@ -2,5 +2,5 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-const app_1 = __importDefault(require("./app"));
+const app_1 = __importDefault(require("../app"));
 module.exports = app_1.default;
