c721627a904af6f99e61888ea4bf7fdde4c5a2c9
YUGAM05 <24002170210108@mail.ljku.edu.in>
2026-05-22 00:55:13 +0530

    fix: export Express app directly using TypeScript export assignment for clean CommonJS wrapper compatibility on Vercel

--- dist/handler.js
+++ dist/handler.js
@@ -2,6 +2,5 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 module.exports = app_1.default;
--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,3 @@
 import app from './app';
 
-module.exports = app;
+export = app;
