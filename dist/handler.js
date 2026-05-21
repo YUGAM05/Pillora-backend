d6942adfb603f9cf093ed14c2fed578afe3e8134
YUGAM05 <24002170210108@mail.ljku.edu.in>
2026-05-22 00:48:37 +0530

    fix: export Express app directly using module.exports for Vercel CommonJS handler compatibility

--- dist/handler.js
+++ dist/handler.js
@@ -4,4 +4,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
-exports.default = app_1.default;
+module.exports = app_1.default;
